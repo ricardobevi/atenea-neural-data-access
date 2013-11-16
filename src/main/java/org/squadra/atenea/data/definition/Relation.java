e218bb9751d8c86d608a6e99b52ce3fc73c0412b
facu <facu@localhost>
2013-11-16 13:16:17 -0300

    refactorizacion de OrderExecutor

---
+++ src/main/java/org/squadra/atenea/data/definition/Relation.java
@@ -0,0 +1,11 @@
+package org.squadra.atenea.data.definition;
+
+import org.neo4j.graphdb.RelationshipType;
+
+public class Relation {
+	public static enum Types implements RelationshipType {
+		SENTENCE,
+		DIALOG,
+		VERB
+	}
+}
---
+++ src/main/java/org/squadra/atenea/data/query/QuestionQuery.java
@@ -0,0 +1,110 @@
+package org.squadra.atenea.data.query;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import org.neo4j.cypher.javacompat.ExecutionResult;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.squadra.atenea.base.word.Word;
+import org.squadra.atenea.data.server.Neo4jServer;
+import org.squadra.atenea.parser.model.SimpleSentence;
+
+public class QuestionQuery {
+
+
+	public ArrayList<SimpleSentence> findAnswers(ArrayList<String> words) {
+		
+		ExecutionResult result = findSentencesByKeyWords(words);
+		ArrayList<SimpleSentence> answers = resultToResponseWords(result);
+		return answers;
+	}
+	
+	
+	private ExecutionResult findSentencesByKeyWords(ArrayList<String> words) {
+		
+		Neo4jServer.beginTransaction();
+		
+		String startNodes = "";
+		String matches = "";
+		String where = "";
+		
+		for (int i = 0; i < words.size(); i++) {
+			
+			if (i > 0) {
+				startNodes += " , ";
+				matches += " , ";
+				
+				if (i == 1) {
+					where += " WHERE ";
+				}
+				else if (i > 1) {
+					where += " AND ";
+				}
+				
+				where += " relation" + (i-1) + ".sentenceId = relation" + i + ".sentenceId ";
+			}
+			
+			startNodes += " node" + i + " = node:words('name:\"" + words.get(i) + "\"') ";
+			matches += "(node" + i + ")-[relation" + i + ":SENTENCE]->() ";
+		}
+		
+		String query = 
+				
+				  " START "
+				+ 		startNodes
+				+ " MATCH "
+				+ 		matches
+				+   where
+				+ " WITH "
+				+ "     relation0 "
+				+ " START "
+				+ "   	startNode = node:words('*:*') "
+				+ " MATCH "
+				+ "     (startNode)-[relation:SENTENCE]->(endNode) "
+				+ " WHERE "
+				+ "     relation.sentenceId = relation0.sentenceId "
+				+ " RETURN "
+				+ "		DISTINCT startNode, relation, endNode "
+				+ " ORDER BY "
+				+ "     relation.sentenceId, relation.sequence ASC; ";
+		
+		System.out.println(query);
+		
+		ExecutionResult result = Neo4jServer.engine.execute(query);
+		return result;
+		
+	}
+	
+	
+	private ArrayList<SimpleSentence> resultToResponseWords(ExecutionResult result) {
+		
+		ArrayList<SimpleSentence> responses = new ArrayList<>();
+		ArrayList<Word> response = new ArrayList<>();
+		
+		boolean firstRel = true;
+		
+		for ( Map<String, Object> row : result )
+		{
+			if (firstRel) {
+				Node startNode = (Node) row.get("startNode");
+				response.add(Neo4jServer.nodeToWord(startNode));
+				firstRel = false;
+			}
+			
+			Node endNode = (Node) row.get("endNode");
+			
+			Word word = Neo4jServer.nodeToWord(endNode);
+			response.add(word);
+			
+			if (word.getName().equals(".")) {
+				responses.add(new SimpleSentence(response));
+				response.clear();
+				firstRel = true;
+			}
+		}
+		return responses;
+	}
+	
+	
+}
